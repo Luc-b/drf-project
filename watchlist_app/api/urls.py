b5bbaecaae0ca5c620c8d9bb4230e0921bb3e8b2
Luc-b <lucijanabubnjic@gmail.com>
2024-11-24 18:01:51 +0100

    view classes

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 # from watchlist_app.api.views import movie_list, movie_details
-from watchlist_app.api.views import WatchListAV, WatchDetailAV, StreamPlatformAV, StreamDetailAV, ReviewList, ReviewDetail
+from watchlist_app.api.views import (WatchListAV, WatchDetailAV,
+                                    StreamPlatformAV, StreamDetailAV,
+                                    ReviewList, ReviewDetail)
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
@@ -9,6 +11,9 @@ urlpatterns = [
     path('stream/', StreamPlatformAV.as_view(), name='stream'),
     path('stream/<int:pk>', StreamDetailAV.as_view(), name='stream-details'),
     
-    path('stream/<int:pk>/rewiew', StreamPlatformAV.as_view(), name='stream-detail'),
-    path('stream/review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
+    path('review/', ReviewList.as_view(), name='review-list'),
+    path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
+    
+    #path('stream/<int:pk/review>', ReviewList.as_view(), name='review-list'),
+    #path('stream/review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),    
 ]
