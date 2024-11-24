93c1e64fec16256afb0d093b56adc3ee3e5940f3
Luc-b <lucijanabubnjic@gmail.com>
2024-11-24 17:27:27 +0100

    serializers

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 # from watchlist_app.api.views import movie_list, movie_details
-from watchlist_app.api.views import WatchListAV, WatchDetailAV, StreamPlatformAV, StreamDetailAV
+from watchlist_app.api.views import WatchListAV, WatchDetailAV, StreamPlatformAV, StreamDetailAV, ReviewList, ReviewDetail
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
     path('<int:pk>', WatchDetailAV.as_view(), name='movie-details'),
     
     path('stream/', StreamPlatformAV.as_view(), name='stream'),
-    path('stream/<int:pk>', StreamDetailAV.as_view(), name='stream-details')
+    path('stream/<int:pk>', StreamDetailAV.as_view(), name='stream-details'),
+    path('review/', ReviewList.as_view(), name='review-list'),
+    path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
 ]
