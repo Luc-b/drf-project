cc8bb88e8b4f35fce03b555f725b646d6a26e52c
Luc-b <lucijanabubnjic@gmail.com>
2024-11-25 21:46:29 +0100

    queryset update

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 # from watchlist_app.api.views import movie_list, movie_details
 from watchlist_app.api.views import (WatchListAV, WatchDetailAV,
                                     StreamPlatformAV, StreamDetailAV,
-                                    ReviewList, ReviewDetail)
+                                    ReviewList, ReviewDetail, ReviewCreate)
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
@@ -14,6 +14,7 @@ urlpatterns = [
     # path('review/', ReviewList.as_view(), name='review-list'),
     # path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
     
-    path('stream/<int:pk>/review', ReviewList.as_view(), name='review-list'),
-    path('stream/review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),    
+    path('stream/<int:pk>/review-create', ReviewCreate.as_view(), name='review-create'),
+    path('stream/<int:pk>/review/', ReviewList.as_view(), name='review-list'),
+    path('stream/review/<int:pk>/', ReviewDetail.as_view(), name='review-detail'),    
 ]
