9e160e74cb94c887e178386626356e96d67ebac9
Luc-b <lucijanabubnjic@gmail.com>
2024-11-26 15:35:23 +0100

    temp login and logout

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -24,7 +24,7 @@ class ReviewCreate(generics.CreateAPIView):
         review_queryset = Review.objects.filter(watchlist=watchlist, review_user=review_user)
         
         if review_queryset.exists():
-            raise ValidationError("You have already reviewed this movie.")
+            raise ValidationError("You  have already reviewed this movie.")
         
         serializer.save(watchlist=watchlist, review_user=review_user)
     
