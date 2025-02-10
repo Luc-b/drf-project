d8970f020d0e4c33d2ccaa7ebbd4790ba0d481ab
Luc-b <136970370+Luc-b@users.noreply.github.com>
2025-02-10 20:13:57 +0100

    Update models.py

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import User
-# Create your models here.
 
 class StreamPlatform(models.Model):
     name = models.CharField(max_length=30)
