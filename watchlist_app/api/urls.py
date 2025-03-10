f5bf935ca7446a576a8534812e9c0f593f072eef
Luc-b <lucijanabubnjic@gmail.com>
2025-03-10 20:26:22 +0100

    swagger ui implemented

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -2,10 +2,29 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from watchlist_app.api import views
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Dokumentacija",
+        default_version="v1",
+        description="Ovdje je opis tvog API-ja.",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="tvojemail@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 router = DefaultRouter()
 router.register('stream', views.StreamPlatformVS, basename='streamplatform')
 
 urlpatterns = [
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+     
     path('', views.WatchListAV.as_view(), name='movie-list'),
     path('<int:pk>/', views.WatchDetailAV.as_view(), name='movie-details'),
     
